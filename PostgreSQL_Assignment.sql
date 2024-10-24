83b8b1beb1852ccad2f4358787186db0cce252a6
Forhad96 <forhadairdrop@gmail.com>
2024-10-24 08:20:53 +0530

    created course sample data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -2,8 +2,6 @@
 
 -- Create database
 create DATABASE university_db
-
-
 -- Creating a student table
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,
@@ -14,7 +12,6 @@ CREATE TABLE students (
     backend_mark INTEGER,
     status VARCHAR(25)
 )
-
 -- Creating a course table
 CREATE TABLE course (
     course_id SERIAL PRIMARY KEY,
@@ -22,83 +19,110 @@ CREATE TABLE course (
     credits INTEGER
 )
 
+
+-- creating sample course
+INSERT INTO course (
+    course_id,
+    course_name,
+    credits
+)
+VALUES(1, 'Next.js', 3),
+(2, 'React.js', 4),
+(3, 'Databases', 3),
+(4, 'Prisma', 3);
+
 -- Creating a enrollment table
 CREATE TABLE enrollment (
     enrollment_id SERIAL PRIMARY KEY,
     student_id INTEGER REFERENCES (student_id),
     course_id INTEGER REFERENCES (course_id)
 )
-
-
 -- Inserting sample student data
 
-INSERT INTO students (
-        student_id,
+INSERT INTO
+    students (
         student_name,
         age,
         email,
         frontend_mark,
         backend_mark,
         status
-    )VALUES (
-        6,
-        'Fiona Green',
-        19,
-        'fiona.green@gmail.com',
-        75,
-        80,
-        NULL
-    ),
-    (
-        7,
-        'George White',
+    )
+VALUES (
+        'Sammer',
         21,
-        'george.white@yahoo.com',
-        68,
-        72,
+        'sammer@example.com',
+        48,
+        60,
         NULL
     ),
     (
-        8,
-        'Hannah Black',
-        22,
-        'hannah.black@outlook.com',
-        82,
-        85,
-        NULL
-    ),
-    (
-        9,
-        'Ian Blue',
-        20,
-        'ian.blue@icloud.com',
-        90,
-        88,
-        NULL
-    ),
-    (
-        10,
-        'Jane Red',
+        'Zoya',
         23,
-        'jane.red@protonmail.com',
-        77,
-        79,
+        'zoya@example.com',
+        52,
+        58,
+        NULL
+    ),
+    (
+        'Nabil',
+        22,
+        'nabil@example.com',
+        37,
+        46,
+        NULL
+    ) (
+        'Rafi',
+        24,
+        'rafi@example.com',
+        41,
+        40,
+        NULL
+    ),
+    (
+        'Shophia',
+        22,
+        'sophia@example.com',
+        50,
+        52,
+        NULL
+    ),
+    (
+        'Hasan',
+        23,
+        'hasan@gamil.com',
+        43,
+        39,
         NULL
     );
-
 
 -- start solving query
 
 -- Query 1:-
 -- Insert a new student record with the following details:
 
-INSERT INTO students(
-student_name,age,email,frontend_mark,backend_mark,status
-)VALUES('Forhad',29,'forhadairdrop@gmail.com',40,50,NULL)
+INSERT INTO
+    students (
+        student_name,
+        age,
+        email,
+        frontend_mark,
+        backend_mark,
+        status
+    )
+VALUES (
+        'Forhad',
+        29,
+        'forhadairdrop@gmail.com',
+        40,
+        50,
+        NULL
+    )
+    -- Query 2:
+    -- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
 
+SELECT * FROM students
 
-    SELECT * FROM students
+SELECT * FROM course
 
-    
-    DROP TABLE students
-
+DROP TABLE students
